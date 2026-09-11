353d88a8fb9979d5ef0f8db8b257eb9c36d916e9
MicaUwU <micaelalujan387@gmail.com>
2026-09-11 16:56:56 -0300

    actualizadox6

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/torneo')
+const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/torneo';
+mongoose.connect(mongoURI)
     .then(() => console.log('¡Conectado a MongoDB con éxito!'))
     .catch(err => console.error('Error al conectar a MongoDB:', err));
 
